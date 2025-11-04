7e978acea4b9b9a9b64600897fa4af4abe275b00
Tago-F <snake112381@gmail.com>
2025-11-04 09:53:04 +0900

    CORS 設定の調整

--- backend/src/main/java/com/tagoapp/backend/config/WebConfig.java
+++ backend/src/main/java/com/tagoapp/backend/config/WebConfig.java
@@ -10,10 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // /api/ で始まるすべてのパスに適用
-                .allowedOrigins(
-                        "http://localhost:4200",
-                        "http://127.0.0.1:4200" // [TODO] ここに本番環境の URL も追加
-                )
+                .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // OPTIONSとDELETEを明示的に許可
                 .allowedHeaders("*") // すべてのヘッダーを許可
                 .allowCredentials(true); // クレデンシャル情報の送信を許可
